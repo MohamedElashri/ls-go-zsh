7629c09106bd877d2bcac2db13ff7f9d995c3299
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-10-28 00:06:43 -0400

    Create ls-go-zsh.plugin.zsh

---
+++ ls-go-zsh.plugin.zsh
@@ -0,0 +1,26 @@
+# Project:  ls-go
+# File:     ls-go-plugin.zsh
+# Author:   Mohamed Elashri
+# Email:    muhammadelashri@gmail.com
+
+
+if ! (( $+commands[ls-go] )); then
+  print "ls-go-plugin: ls-go not found on path. Please install ls-go before using this plugin." >&2
+  return 1
+fi
+
+# general use aliases 
+alias ls='ls-go' # just replace ls by ls-go and allow all other ls-go arguments
+alias lsa='ls-go -all' # show all including hidden files
+alias ll='ls-go -l' # long, include size, date, owner, and permissions
+alias llm='ls-go -L' # show paths for symlinks
+alias lls='ls-go -R' # show paths for symlinks
+alias lsa='ls-go -S' # show statistics
+alias lx='ls-go -recurse' # traverse all dirs recursively
+alias ls-time='ls-go --time' # sort by time
+alias ls-kind='ls-go --kind' # sort by kind
+alias lS='ls-go -f' # show files only
+
+
+
+
